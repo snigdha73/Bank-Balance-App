d735bc7f6506005156c7650a7db288b81682a745
Snigdha Singh <snigdha73.2000@gmail.com>
2021-06-02 11:56:05 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -31,11 +31,11 @@ function App() {
                <img src="user.png" alt="user" className="user"></img>
                <div className="form-group col-12 flex-column "> </div>
                <div className="row">
-                        <div className="form-group col-6  "> <label className="form-control-label px-3">YOUR BALANCE</label>  </div><div className="form-group col-sm-6"> <button disabled className="btn-block btn-light">{count}</button> </div>
+                        <div className="form-group col-6 " id="balance"> <button disabled className="btn-block btn-light">YOUR BALANCE</button>  </div><div className="form-group col-sm-6"> <button disabled className="btn-block btn-light">{count}</button> </div>
                     </div>
                 
                     <div className="row">
-                        <div className="form-group col-6  "> <label className="form-control-label px-3">ADD 10,000</label>  </div><div className="form-group col-sm-6"> <button type="button" className="btn-block btn-success"   onClick={() => dispatch(increment())}>+ 10,000</button> </div>
+                        <div className="form-group col-6 "> <label className="form-control-label px-3">ADD 10,000</label>  </div><div className="form-group col-sm-6"> <button type="button" className="btn-block btn-success"   onClick={() => dispatch(increment())}>+ 10,000</button> </div>
                     </div>
                     <div className="row">
                         <div className="form-group col-6"> <label className="form-control-label px-3">NEGATE 10,000</label>  </div><div className="form-group col-sm-6"> <button type="button" className="btn-block btn-secondary"  onClick={() => dispatch(decrement())}>- 10,000</button> </div>
